d5ee5bf04bac03b72c7b5574a4df483da94ef55c
unknown <raffyamoguis@gmail.com>
2021-06-03 15:15:21 +0800

    remove bounce animation on view button

--- js/img-overlay.js
+++ js/img-overlay.js
@@ -8,10 +8,9 @@ $('.work-btn').attr('title', 'View');
 $('.work-card').hover(function(){
     var button = $(this).find('.work-btn');
     button.show();
-    button.effect('bounce');
+    // button.effect('bounce');
 }, function(){
     var button = $(this).find('.work-btn');
-    // button.stop(true, true).effect("bounce", { times: 1 }, 1);
-    button.finish().effect('bounce', 1000);
+    // button.finish().effect('bounce', 1000).hide().fadeIn();
     button.hide();
 });
